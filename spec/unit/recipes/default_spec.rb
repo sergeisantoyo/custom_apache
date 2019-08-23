1038f4276dba215f3d9df44808c55028de9d73f8
sergeisantoyo <sergeisantoyo@gmail.com>
2019-08-23 17:10:43 -0500

    Tests were ran and passed correctly yes

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,11 +7,11 @@
 require 'spec_helper'
 
 describe 'custom_apache::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  context 'When all attributes are default, on Ubuntu 16.04' do
+  let(:chef_run) do
+    runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+    runner.converge(described_recipe)
+  end
 
     it 'converges successfully' do
       chef_run # This should not raise an error
