98f5bf8236e7008d01737ca1787e0bc0009f07c1
sergeisantoyo <sergeisantoyo@gmail.com>
2019-08-23 17:24:39 -0500

    Final version of the cookbook achieved Bla,Bla,Bla.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,10 +8,10 @@ require 'spec_helper'
 
 describe 'custom_apache::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
-  let(:chef_run) do
-    runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-    runner.converge(described_recipe)
-  end
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
 
     it 'converges successfully' do
       chef_run # This should not raise an error
